736faa0aa8c917f6a070043095c12e6585ecbc33
Rok Garbas <rok@flox.dev>
2025-01-14 16:47:37 +0100

    Read manifest from manifest.lock and show package version in InstallView

---
+++ src/config.ts
@@ -0,0 +1,26 @@
+import * as vscode from 'vscode';
+
+export enum System {
+  AARCH64_LINUX = "aarch64-linux",
+  AARCH64_DARWIN = "aarch64-darwin",
+  X86_64_LINUX = "x86_64-linux",
+  X86_64_DARWIN = "x86_64-darwin",
+}
+
+export type Package = {
+  install_id: string,
+  system: System,
+  version: string,
+  group: string,
+  license: string,
+  description: string,
+  attr_path: string,
+}
+
+export type Packages = Map<System, Map<string, Package>>
+
+export interface View {
+  registerProvider(viewName: string): vscode.Disposable;
+  refresh(): Promise<void>;
+}
+
