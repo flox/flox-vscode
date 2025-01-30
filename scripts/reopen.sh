757fdc5d903b1d88946f8b3132dc32b8b2443ee8
Rok Garbas <rok@flox.dev>
2025-01-30 16:19:24 +0100

    feat: VSCode extension should now also work with vscodium and cursor (#41)
    Also keep VSCode running on activate command. This allows services to be
    started.
    
    Fixes #19
    
    ---------
    
    Co-authored-by: Matthew Kenigsberg <matthew@floxdev.com>

--- scripts/reopen.sh
+++ scripts/reopen.sh
@@ -2,13 +2,10 @@
 
 set -eu -o pipefail
 
-#echo "START" >  /tmp/test.out
-#echo " => PATH: $PWD" >> /tmp/test.out
+editor=${1:-code}
 
-## Get the file descriptor from NODE_CHANNEL_FD
+# Get the file descriptor from NODE_CHANNEL_FD
 fd="${NODE_CHANNEL_FD:-3}"
-
-#echo " => NODE_CHANNEL_FD: $NODE_CHANNEL_FD" >> /tmp/test.out
 
 # Function to send a response back to Node.js
 send() {
@@ -17,16 +14,7 @@ send() {
 
 send '{"action":"close"}'
 
-# Read JSON messages from the file descriptor
 while IFS= read -u "$fd" -r line; do
-
-    # Print the message received for debugging
-    #echo " => MESSAGE: $line" >> /tmp/test.out
-
-    #echo -n " => FLOX ACTIVATE" >> /tmp/test.out
-    flox activate -- bash -c "echo ' => CODE' >> /tmp/test.out && code -n --verbose $PWD" 
-    #&> /tmp/test.out
-
-    # Break after handling the first message
+    flox activate -- bash -c "$editor --new-window --wait --verbose $PWD" 
     break
 done
